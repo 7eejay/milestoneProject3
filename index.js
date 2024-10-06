ec93d0333d5670cc1ea466c8e6fbb2362d8ae8d3
7eejay <tijani.ramon105@gmail.com>
2024-10-06 13:44:33 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let ggg=document.getElementById("boy")
+
+
+let theText=document.getElementsByClassName("des")
+
+console.log(ggg);
+
+const addItemToDisplay=(theText)=>{
+    ggg.value =ggg.value+theText}
+
+    console.log(addItemToDisplay);
+
+
+    // const addToCartButton=document.querySelectorAll('addToCart');
+    // for(let i=0;i< addToCartButton.length;i++) {
+    //     addToCartButton[i].addEventListener('click', handleAddToCart);
+    // }
+
+
+    // const removeCartButton=document.
